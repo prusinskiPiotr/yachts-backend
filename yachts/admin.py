765c8f05b5119d34ba576e19c04c2cf42b8ccd99
Piotr Prusinski <prusinski.pio@gmail.com>
2019-07-14 12:53:17 +0200

    create db schema, manage it from admin page

--- yachts/admin.py
+++ yachts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Yachts, Rentals
 
-# Register your models here.
+
+@admin.register(Yachts)
+class YachtsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'model_name', 'length', 'width', 'year', 'max_crew', 'berths', 'owner')
+
+
+@admin.register(Rentals)
+class RentalsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'from_date', 'to_date', 'insurance', 'yacht', 'user')
+
--- yachts/models.py
+++ yachts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Yachts(models.Model):
+    model_name = models.CharField(max_length=255)
+    length = models.CharField(max_length=10)
+    width = models.CharField(max_length=10)
+    year = models.CharField(max_length=4)
+    max_crew = models.IntegerField()
+    berths = models.IntegerField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Rentals(models.Model):
+    from_date = models.DateField()
+    to_date = models.DateField()
+    insurance = models.BooleanField()
+    yacht = models.ForeignKey(Yachts, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
